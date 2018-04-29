aae0716db4bec46e99fb7affd3c7ed2035a93554
BenHampton <BenHampton11@yahoo.com>
2018-04-29 12:51:54 -0500

    i added gallery.html, removed orderOnline.html bc its in the menu.html/papal, I removed reservations and put a link for reservations in the index.html - when clicked prompts user to send email for there reservations (the email send-to and subject are pre filled along with a message in the body for directions.. at the moment it sends to my email just for testing purposes) in the gallery.html the pics are linked to there respective Instagram links

--- MagnificentMushroom/Assets/JavaScripts/cartCookies.js
+++ MagnificentMushroom/Assets/JavaScripts/cartCookies.js
@@ -1,10 +1,6 @@
-
-
 addEventListener(window, "load", retrieveOrder, false);
 
 function retrieveOrder() {
-
-
 
 	if(document.cookie) {
 		var itemsInCart = new Array();
@@ -27,18 +23,17 @@ function retrieveOrder() {
 
 			var headRow = document.createElement("tr");
 			var productHead = document.createElement("th");
-			productHead.innerHTML - "Product";
-			var descriptionHead = document.createElement("th");
-			descriptionHead.innerHTML = "Description";
+			productHead.innerHTML = "Product";
 			var quantityHead = document.createElement("th");
 			quantityHead.innerHTML = "Qty";
 			var priceHead = document.createElement("th");
 			priceHead.innerHTML = "Price";
 			var alterHead = document.createElement("th");
-			alterHead.innerHTML = "Change";
+			alterHead.innerHTML = "Remove Item";
+			alterHead.style.textAlign = "right";
+
 
 			headRow.appendChild(productHead);
-			headRow.appendChild(descriptionHead);
 			headRow.appendChild(quantityHead);
 			headRow.appendChild(priceHead);
 			headRow.appendChild(alterHead);
@@ -47,6 +42,9 @@ function retrieveOrder() {
 
 			var tbody = document.createElement("tbody");
 			cartTable.appendChild(tbody);
+			var t
+			var num
+			var tot = 0;
 
 			for (var i = 0; i < itemsInCart.length; i++) {
 				var newRow = document.createElement("tr");
@@ -57,17 +55,22 @@ function retrieveOrder() {
 				newRow.appendChild(productCell);
 
 				var quantityCell = document.createElement("td");
-				quantityCell.style.textAlign = "right";
-				quantityCell.innerHTML = getField(itemsInCart[i], "quantity");
+				quantityCell.style.textAlign = "center";
+				var q = getField(itemsInCart[i], "quantity");
+				quantityCell.innerHTML = q;
 				newRow.appendChild(quantityCell);
 
 				var priceCell = document.createElement("td");
-				priceCell.style.textAlign = "right";
-				priceCell.innerHTML = getField(itemsInCart[i], "price");
+				priceCell.style.textAlign = "center";
+				num = getField(itemsInCart[i], "price");
+				t = parseFloat(num.substring(1)) * q;
+				priceCell.innerHTML = "$" + t.toFixed(2);
+				tot += t;
 				newRow.appendChild(priceCell);
 
 				var alterCell = document.createElement("td");
 				var changeButton = document.createElement("input");
+				changeButton.style.float = "right";
 				changeButton.type = "button";
 				changeButton.id = itemsInCart[i];
 				changeButton.className = "deleteItem";
@@ -82,7 +85,15 @@ function retrieveOrder() {
 				newRow.appendChild(alterCell);
 
 				tbody.appendChild(newRow);
-			}
+			}//end of for-loop
+			 document.getElementById("cartTotal").innerHTML = tot.toFixed(2);
+			 document.getElementById("ctotal").value = tot.toFixed(2);
+			 var cartPosition = document.getElementById("cartPosition");
+			 cartPosition.style.fontSize = "25px"
+			 cartPosition.style.float = "right";
+			 cartPosition.style.marginRight = "250px"
+			 cartPosition.style.color = "green";
+			 cartPosition.style.fontWeight = "bold";
 		}
 	}
 }
